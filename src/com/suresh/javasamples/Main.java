3df56dd62cca8aa7ae69241f0f870be01482205d
Suresh Nomula <Suresh Nomula@DESKTOP-EBTKCOS>
2018-11-03 15:47:29 +0530

    immutable class demo

---
+++ src/com/suresh/javasamples/Main.java
@@ -0,0 +1,19 @@
+package com.suresh.javasamples;
+
+public class Main {
+
+	public static void main(String args[]) {
+
+		Age a = new Age();
+		a.setDay(01);
+		a.setMonth(01);
+		a.setYear(2018);
+
+		ImmutableStudent std = new ImmutableStudent(101, "X", a);
+		System.out.println("Alex age year before modification = " + std.getAge().getYear());
+		std.getAge().setYear(1993);
+		System.out.println("Alex age year after modification = " + std.getAge().getYear());
+
+	}
+
+}
